afb59623ae52cee59a20db91ce8f6c07e91c1dab
Desain7 <Desain7@users.noreply.github.com>
2023-09-25 15:21:42 +0000

    deploy: 5637c4a5fcb8b14413fa2af58e2b8fbb6a8e1225

--- assets/chunks/selectAll-8725692e.c7613096.js
+++
@@ -1,1 +0,0 @@
-import{a2 as t,a3 as n,a4 as o}from"../app.ce6aafbf.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
