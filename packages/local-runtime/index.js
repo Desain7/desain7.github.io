aadc812e0efb5b54ccec3892638b11fa86a5490b
Desain7 <1810479856@qq.com>
2023-10-16 19:21:14 +0800

    docs: 添加服务器部署

--- packages/local-runtime/index.js
+++ packages/local-runtime/index.js
@@ -1,31 +1,9 @@
-function findMostWord(article) {
-  // 合法性判断
-  if (!article) return
-  // 参数处理
-  article = article.trim().toLowerCase()
-  const wordList = article.match(/[a-z]+/g)
-  const visited = []
-  let maxNum = 0
-  let maxWord = ''
-  article = ` ${wordList.join('  ')} `
-  console.log(article)
-  // 遍历判断单词出现次数
-  wordList.forEach((item) => {
-    if (visited.indexOf(item) < 0) {
-      // 加入 visited
-      visited.push(item)
-      const word = new RegExp(` ${item} `, 'g')
-      const num = article.match(word).length
-      if (num > maxNum) {
-        maxNum = num
-        maxWord = item
-      }
-    }
-  })
-  return `${maxWord}  ${maxNum}`
+const obj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3'
 }
 
-const arti =
-  'lorem, ipsum dolor sit amet consectetur adipiscing elit ipsum sit sit sit'
+delete obj.key2
 
-console.log(findMostWord(arti))
+console.log(obj) // { key1: 'value1', key3: 'value3' }
