e1848c7b6ed9f99704730dc96b2d7c61c71107f4
Desain7 <1810479856@qq.com>
2024-03-14 21:32:14 +0800

    docs: http 各版本的差异

--- packages/local-runtime/index.js
+++ packages/local-runtime/index.js
@@ -1,33 +1,24 @@
-class EventEmitter {
-  constructor() {
-    this.events = {}
-  }
+let abort
 
-  on(name, callback) {
-    if (this.event[name]) {
-      this.events[name].push(callback)
-    } else {
-      this.events[name] = [callback]
-    }
-  }
-
-  emit(name, ...args) {
-    if (!this.events[name]) {
-      return
-    }
-    this.events[name].forEach((cb) => {
-      cb(...args)
-    })
-  }
-
-  off(name, callback) {
-    if (!this.events[name]) {
-      return
-    }
-    if (callback) {
-      this.events[name].filter((item) => item !== callback)
-    } else {
-      delete this.events[name]
-    }
-  }
+function abortPromise(p) {
+  // 通过 Promise.race 实现中断
+  return Promise.race([p, new Promise((resolve, reject) => (abort = reject))])
 }
+
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  }, 2000)
+})
+
+const ap = abortPromise(p)
+  .then((res) => {
+    console.log('success')
+  })
+  .catch((err) => {
+    console.log('abort')
+  })
+
+setTimeout(() => {
+  abort()
+}, 1000)
