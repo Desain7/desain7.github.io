1596f93b062fd0e70dcbda772a74744f73e9de47
Desain7 <1810479856@qq.com>
2025-04-10 21:52:23 +0800

    chore: 更新 cname

--- packages/local-runtime/index.js
+++ packages/local-runtime/index.js
@@ -1,24 +1,4 @@
-let abort
-
-function abortPromise(p) {
-  // 通过 Promise.race 实现中断
-  return Promise.race([p, new Promise((resolve, reject) => (abort = reject))])
+function a() {
+  console.log(this)
 }
-
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('success')
-  }, 2000)
-})
-
-const ap = abortPromise(p)
-  .then((res) => {
-    console.log('success')
-  })
-  .catch((err) => {
-    console.log('abort')
-  })
-
-setTimeout(() => {
-  abort()
-}, 1000)
+a.call(null)
